2fbab437b6d0fdf85cccd23e452ea63451e94bea
parkeralexjm <parkeralexjm@hotmail.co.uk>
2022-07-29 10:50:59 +0100

    Created add, subtract, multiply and divide functions
    Created the functions inside the javascript file and tested using
    console log in the index.html file

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = function() {
+	return (arguments[0] + arguments[1])
+};
+
+const subtract = function() {
+	return (arguments[0] - arguments[1])
+};
+
+const multiply = function(array) {
+	return (arguments[0] * arguments[1])
+};
+
+const divide = function() {
+    return (arguments[0] / arguments[1])
+}
