530f80581f9b922277eb2fe7d4dc673ff2db7c76
parkeralexjm <parkeralexjm@hotmail.co.uk>
2022-07-29 11:10:17 +0100

    Created operate function
    Created the operate function to take 2 numbers and an operator and
    return the result (input for the operator must be a string not a
    literal)

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// For each function accept two arguments and apply the operator according to
+// the function title.
+
 const add = function() {
-	return (arguments[0] + arguments[1])
+    return (arguments[0] + arguments[1])
 };
 
 const subtract = function() {
@@ -13,3 +16,29 @@ const multiply = function(array) {
 const divide = function() {
     return (arguments[0] / arguments[1])
 }
+
+// Function takes two numbers and an operator as input then calls the 
+// appropriate function.
+const operate = function(a, b, operator) {
+    // If the operator is add then perform this action
+    console.log(a)
+    console.log(b)
+    console.log(operator)
+    
+    if (operator == 'add') {
+        return add (a, b);
+    }
+    // If the operator is subtract then perform this action
+    else if (operator == 'subtract') {
+        return subtract (a, b);
+    } 
+    // If the operator is multiply then perform this action
+    else if (operator == 'multiply') {
+        return multiply (a, b);
+    } 
+    // If the operator is divide then perform this action
+    else {
+        return divide (a, b);
+    }
+    
+};
